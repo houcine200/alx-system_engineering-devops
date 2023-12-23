0b6720e653a2ae6cad3ebd1dd00785cb84a033a9
Houcine Walaq <houcine.walaq@gmai.com>
2023-12-23 15:32:15 +0100

    100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Puppet script to generate ssh config file
+
+# Ensure SSH client configuration to disable password authentication
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+# Ensure SSH client configuration to declare the identity file
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
