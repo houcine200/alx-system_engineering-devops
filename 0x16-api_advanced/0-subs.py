1f6b7971365165a35abf99593b201502d670915d
Houcine Walaq <houcine.walaq@gmai.com>
2024-02-13 16:21:01 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
         return 0
 
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
