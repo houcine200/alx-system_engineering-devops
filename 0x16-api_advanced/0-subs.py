0cdaf0d7217aac55a61e7cc9d469076313cb97be
Houcine Walaq <houcine.walaq@gmai.com>
2024-02-13 18:46:10 +0100

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,7 +16,7 @@ def number_of_subscribers(subreddit):
 
     if response.status_code == 200:
         data = response.json()
-        subscribers = data.get('data').get('subscribers')
+        subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
