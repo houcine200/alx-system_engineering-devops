9f8773a1c3bef14db6368ef2ed591e6161ee699e
Houcine Walaq <houcine.walaq@gmai.com>
2024-02-14 12:48:17 +0100

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # Fix 500 Internal Server Error returned by the Apache server.
 exec {'replace':
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
 }
